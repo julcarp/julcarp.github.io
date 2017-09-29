f7ed8d49cd23a4b04c0622381223562af494ebc6
Juliet Carpenter <julietmcarpenter@gmail.com>
2017-09-28 21:39:31 -0400

    Adding files

---
+++ scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $("a").click(function (event) {
+   event.preventDefault();
+       var targetID = $(this).attr('href');
+        $('html,body').animate({
+         scrollTop: $(targetID).offset().top
+        }, 1000, 'swing');
+     });
+});
